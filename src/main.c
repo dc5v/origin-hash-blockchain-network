46150d664a520ed664298ef5f8538fdb24eb0d60
dc5v <dev@dc5v.com>
2024-07-24 02:00:50 +0900

    daemon 만드는중

--- src/main.c
+++ src/main.c
@@ -1,48 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
 #include "blockchain/blockchain.h"
 #include "blockchain/block.h"
 #include "transaction/transaction.h"
+#include "network/network.h"
+#include "util/util.h"
 
-int main()
+#define PORT 44333
+
+int main(int argc, char *argv[])
 {
   Blockchain *blockchain = create_blockchain();
-  Block *new_block = block_create(1, blockchain->blocks[blockchain->block_count - 1]->hash);
+  Block *new_block = create_block(1, blockchain->blocks[blockchain->block_count - 1]->hash);
 
   block_transaction_push(new_block, "Alice contributed code to project X");
   add_block(blockchain, new_block);
 
   if (blockchain_validate(blockchain))
   {
-    printf("Blockchain valid\n");
+    logmsg("Blockchain is valid.");
+    logmsgf("Blockchain is valid.");
   }
   else
   {
-    printf("Blockchain invalid.\n");
+    logmsg("Blockchain is invalid.");
+    logmsgf("Blockchain is invalid.");
   }
 
   for (int i = 0; i < blockchain->block_count; i++)
   {
     Block *block = blockchain->blocks[i];
 
-    printf("Block %d:\n", block->index);
-    printf("Timestamp: %ld\n", block->epoch);
-    printf("Previous Hash: %s\n", block->hash_prev);
-    printf("Hash: %s\n", block->hash);
-    printf("Transactions:\n");
+    logmsg("Block %d:", block->index);
+    logmsg("Timestamp: %ld", block->epoch);
+    logmsg("Previous Hash: %s", block->hash_prev);
+    logmsg("Hash: %s", block->hash);
+    logmsg("Transactions:");
 
     for (int j = 0; j < MAX_TRANSACTIONS; j++)
     {
       if (strlen(block->transactions[j]) > 0)
       {
-        printf("  %s\n", block->transactions[j]);
+        logmsg("  %s", block->transactions[j]);
       }
     }
 
-    printf("\n");
+    logmsg("");
   }
 
+  create_server(PORT);
   blockchain_free(blockchain);
 
   return 0;
